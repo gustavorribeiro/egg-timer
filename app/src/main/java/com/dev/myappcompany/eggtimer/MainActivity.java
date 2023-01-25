0e4d273e5c8259a1c3f3f1f005353c98c94bfad4
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-01-25 14:01:29 -0300

    adding time in timer

--- app/src/main/java/com/dev/myappcompany/eggtimer/MainActivity.java
+++ app/src/main/java/com/dev/myappcompany/eggtimer/MainActivity.java
@@ -3,6 +3,9 @@ package com.dev.myappcompany.eggtimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = findViewById(R.id.timerSeekBar);
+        final TextView timerTextView = findViewById(R.id.countdownTextView);
+
+        int maximumTime = 600;
+        int minimumTime = 30;
+
+        timerSeekBar.setMax(maximumTime);
+        timerSeekBar.setProgress(minimumTime);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int minutes = progress / 60;
+                int seconds = progress - minutes * 60;
+
+                timerTextView.setText(Integer.toString(minutes) + ":" + Integer.toString(seconds));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
